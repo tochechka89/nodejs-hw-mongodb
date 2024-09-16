03e6ec5e168cb06e026605c7565126cb76772ced
tochechka89 <rabota20ton@gmail.com>
2024-09-16 17:51:57 +0300

    ver2

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -8,7 +8,7 @@ export const initMongoConnection = async () => {
         const password = env("MONGODB_PASSWORD");
         const url = env("MONGODB_URL");
         const db = env("MONGODB_DB");
-        const DB_HOST = "mongodb+srv://${user}:${password}@${url}/${db}?retryWrites=true&w=majority&appName=Cluster0"
+        const DB_HOST = `mongodb+srv://${user}:${password}@${url}/${db}?retryWrites=true&w=majority&appName=Cluster0`;
         await mongoose.connect(DB_HOST);
         console.log("Mongo connection successfully established!");
     }
