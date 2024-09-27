3e76af4441f7f6c4c57cee2aa16ed3bb9a08586a
tochechka89 <rabota20ton@gmail.com>
2024-09-27 20:45:31 +0300

    ver4
    joi

--- src/validation/contact.js
+++ src/validation/contact.js
@@ -11,7 +11,7 @@ export const contactAddSchema = Joi.object({
 });
 
 export const contactPatchSchema = Joi.object({
-    name: Joi.string.min(3).max(20),
+    name: Joi.string().min(3).max(20),
     phoneNumber: Joi.string().min(3).max(20),
     email: Joi.string().min(3).max(20),
     isFavourite: Joi.boolean(),
