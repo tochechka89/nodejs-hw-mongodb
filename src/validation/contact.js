61440199d5f6f4860579d5109aff59e5c7699aac
tochechka89 <rabota20ton@gmail.com>
2024-09-27 20:26:43 +0300

    ver2

--- src/validation/contact.js
+++ src/validation/contact.js
@@ -1,6 +1,6 @@
 import Joi from "joi";
 
-import { enumList } from "../constants/contacts.js";
+import { enumList } from "../constants/contact.js";
 
 export const contactAddSchema = Joi.object({
     name: Joi.string().min(3).max(20).required(),
