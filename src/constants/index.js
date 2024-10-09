9354e822be66b3ae0117494569c8010729050889
tochechka89 <rabota20ton@gmail.com>
2024-10-10 00:56:38 +0300

    ver2

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,6 +2,14 @@ import path from 'node:path';
 
 export const SORT_ORDER = ["asc", "desc"];
 
+export const SMTP = {
+    SMTP_HOST: 'SMTP_HOST',
+    SMTP_PORT: 'SMTP_PORT',
+    SMTP_USER: 'SMTP_USER',
+    SMTP_PASSWORD: 'SMTP_PASSWORD',
+    SMTP_FROM: 'SMTP_FROM',
+};
+
 export const TEMPLATES_DIR = path.resolve("src", "templates");
 
 export const TEMP_UPLOAD_DIR = path.resolve("temp");
