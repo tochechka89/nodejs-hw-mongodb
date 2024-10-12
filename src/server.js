f772cc52244d7b80a98d401e435c13322cfcccb1
tochechka89 <rabota20ton@gmail.com>
2024-10-12 17:24:16 +0300

    ver1

--- src/server.js
+++ src/server.js
@@ -6,10 +6,12 @@ import { env } from "./utils/env.js";
 
 import notFoundHandler from "./middlewares/notFoundHandler.js";
 import errorHandler from "./middlewares/errorHandler.js";
+import swaggerDocs from "./middlewares/swaggerDocs.js";
 //import logger from "./middlewares/logger.js";
 
 import contactsRouter from "./routers/contacts.js";
 import authRouter from "./routers/auth.js";
+import { UPLOAD_DIR } from './constans/index.js';
 
 export const setupServer = () => {
     const app = express();
@@ -20,14 +22,15 @@ export const setupServer = () => {
     app.use(cookieParser());
     app.use(express.static("uploads"));
 
+    app.use("/uploads", express.static(UPLOAD_DIR));
     app.use("/auth", authRouter);
     app.use("/contacts", contactsRouter);
+    app.use("/api-docs", swaggerDocs());
 
     app.use(notFoundHandler);
 
     app.use(errorHandler);
 
     const port = Number(env("PORT", 3000));
-
     app.listen(port, () => console.log(`Server is running on port ${port}`));
 };
